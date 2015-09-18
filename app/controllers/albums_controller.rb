e4303253f9876afec146180289194a9c62448d87
Leah Drake <drake.leah@gmail.com>
2015-09-17 18:11:07 -0700

    Create forms to add Band and Album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,24 @@
 class AlbumsController < ApplicationController
+
+  def new
+    @album = Album.new
+    @album.band = Band.find(params[:band_id])
+    render :new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to album_url(@album)
+    end
+  end
+
+  def update
+  end
+
+  private
+  def album_params
+    params.require(:albums).permit(:band_id, :title, :recording_type)
+  end
+
 end
